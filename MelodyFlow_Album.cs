7d3ea97c112eb96e4a75a84ccb4f9ff11ab27ea1
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-03-10 13:37:57 -0300

    MelodyFlow_Album.cs
    Programa responsável por criar os Álbuns do Projeto Principal.

---
+++ MelodyFlow_Album.cs
@@ -0,0 +1,30 @@
+namespace MelodyFlow.Models;
+
+class Album
+{
+    private List<Music> songs = new List<Music>();
+
+    public Album(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+    public int TotalDuration => songs.Sum(m => m.Duration);
+    public List<Music> Songs => songs;
+
+    public void AddSong(Music song)
+    {
+        songs.Add(song);
+    }
+
+    public void DisplayAlbumSongs()
+    {
+        Console.WriteLine($"{Name}'s Songs:\n");
+        foreach (var song in songs)
+        {
+            Console.WriteLine($"Song: {song.Name}");
+        }
+        Console.WriteLine($"\nListening to the entire Almbum takes {TotalDuration}");
+    }
+}
