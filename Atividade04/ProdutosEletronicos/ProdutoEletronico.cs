72b499a1ff6aec45e0cbf4a9b44c24d551b27c07
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-03-12 18:12:03 -0300

    ProdutoEletronico.cs
    Classe responsável pela construção do Core de um Produto Eletrônico na Atividade 04.

---
+++ Atividade04/ProdutosEletronicos/ProdutoEletronico.cs
@@ -0,0 +1,12 @@
+namespace Atividade03.ProdutosEletronicos;
+
+internal class ProdutoEletronico
+{
+    public string Modelo { get; set; }
+    public double Preco { get; set; }
+
+    public virtual string ExibirInformacoes()
+    {
+        return $"Modelo: {Modelo}, Preço: {Preco:C}";
+    }
+}
