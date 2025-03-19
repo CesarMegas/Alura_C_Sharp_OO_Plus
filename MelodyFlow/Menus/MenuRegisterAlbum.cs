2bee9ca43dbc152dd02cd75c82ec3f5f27ec6b4a
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-03-19 18:30:07 -0300

    MenuRegisterAlbum.cs
    Menu responsável pelo Registro dos Álbuns do Programa Principal.

--- MelodyFlow/Menus/MenuRegisterAlbum.cs
+++ MelodyFlow/Menus/MenuRegisterAlbum.cs
@@ -2,29 +2,26 @@ using MelodyFlow.Models;
 
 namespace MelodyFlow.Menus;
 
-internal class MenuViewBandDetails : Menu
+internal class MenuRegisterAlbum : Menu
 {
-    //Function that Runs the Menu that Views the Bands Details
-    public override void Run(Dictionary<string, Models.Band> bandsDictionary)
+    // Function that Registers a Band's Album
+    public override void Run(Dictionary<string, Band> bandsDictionary)
     {
         base.Run(bandsDictionary);
-        DisplayOptionTitle("View the Details of a Band");
-        // Receives the Name of the Band typed by the User
-        Console.Write("Enter the Name of the Band you want to view the details: ");
+        DisplayOptionTitle("Register a Band's Album");
+        // Requests the Name of the Band the User wants to register an album to
+        Console.WriteLine("Enter the Band whose Album you want to Register: ");
         string bandName = Console.ReadLine()!;
 
-        // If the Band is found in the Band Dictionary, Displays their Reviews Average
+        // If the Band is found in the Band Dictionary, Registers the Album
         if (bandsDictionary.ContainsKey(bandName))
         {
+            Console.WriteLine("\nNow Enter the Album's Title: ");
+            string albumTitle = Console.ReadLine()!;
             Band band = bandsDictionary[bandName];
-            Console.WriteLine($"\nThe Average of {bandName}'s Reviews is {band.Average}.");
-            Console.WriteLine($"\n{bandName}'s Discography: ");
-            foreach(Album album in band.Albums)
-            {
-                Console.WriteLine($"· {album.Name} -> {album.Average}");
-            }
-            Console.WriteLine("\nType a Key to Return to the Main Menu");
-            Console.ReadKey();
+            band.AddAlbum(new Album(albumTitle));
+            Console.WriteLine($"The Album {albumTitle} from {bandName} was successfully registered!");
+            Thread.Sleep(3000);
             Console.Clear();
         }
         // Else, displays an Error message and returns to the Main Menu
