0058076e4f7edabf4b0df0443e64ccc1d6b14bf4
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-03-19 18:28:21 -0300

    MenuRegisterAlbum.cs
    Programa responsável pela exibição do Menu que Registra os Álbuns do Projeto Principal.

--- MelodyFlow/Menus/MenuRegisterAlbum.cs
+++ MelodyFlow/Menus/MenuRegisterAlbum.cs
@@ -2,26 +2,29 @@ using MelodyFlow.Models;
 
 namespace MelodyFlow.Menus;
 
-internal class MenuRegisterAlbum : Menu
+internal class MenuViewBandDetails : Menu
 {
-    // Function that Registers a Band's Album
-    public override void Run(Dictionary<string, Band> bandsDictionary)
+    //Function that Runs the Menu that Views the Bands Details
+    public override void Run(Dictionary<string, Models.Band> bandsDictionary)
     {
         base.Run(bandsDictionary);
-        DisplayOptionTitle("Register a Band's Album");
-        // Requests the Name of the Band the User wants to register an album to
-        Console.WriteLine("Enter the Band whose Album you want to Register: ");
+        DisplayOptionTitle("View the Details of a Band");
+        // Receives the Name of the Band typed by the User
+        Console.Write("Enter the Name of the Band you want to view the details: ");
         string bandName = Console.ReadLine()!;
 
-        // If the Band is found in the Band Dictionary, Registers the Album
+        // If the Band is found in the Band Dictionary, Displays their Reviews Average
         if (bandsDictionary.ContainsKey(bandName))
         {
-            Console.WriteLine("\nNow Enter the Album's Title: ");
-            string albumTitle = Console.ReadLine()!;
             Band band = bandsDictionary[bandName];
-            band.AddAlbum(new Album(albumTitle));
-            Console.WriteLine($"The Album {albumTitle} from {bandName} was successfully registered!");
-            Thread.Sleep(3000);
+            Console.WriteLine($"\nThe Average of {bandName}'s Reviews is {band.Average}.");
+            Console.WriteLine($"\n{bandName}'s Discography: ");
+            foreach(Album album in band.Albums)
+            {
+                Console.WriteLine($"· {album.Name} -> {album.Average}");
+            }
+            Console.WriteLine("\nType a Key to Return to the Main Menu");
+            Console.ReadKey();
             Console.Clear();
         }
         // Else, displays an Error message and returns to the Main Menu
