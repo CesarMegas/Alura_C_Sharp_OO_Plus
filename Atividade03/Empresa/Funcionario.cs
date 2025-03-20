21187900bc2df786b06badf54a8c1e4323aa2545
César Nagy <103067057+CesarMegas@users.noreply.github.com>
2025-03-19 21:10:07 -0300

    INotificavel.cs
    Interface responsável por definir um padrão aos Programas Notificáveis do Projeto Principal.

--- Atividade03/Empresa/Funcionario.cs
+++
@@ -1,7 +0,0 @@
-namespace Atividade03.Empresa;
-
-internal class Funcionario
-{
-    public string Nome { get; set; }
-    public double Salario { get; set; }
-}
---
+++ Atividade04/Notificacao/INotificavel.cs
@@ -0,0 +1,6 @@
+namespace Atividade04.Notificacao;
+
+internal interface INotificavel
+{
+    void EnviarNotificacao();
+}
